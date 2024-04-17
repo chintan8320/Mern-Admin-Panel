6e453fd634561d7d72ffdbace9c1359f8c9a02fe
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-04-18 00:46:11 +0530

    intial register api

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const dbConnect = require('./dbConnect.js')
 const dotenv = require('dotenv');
 const app = express();
 
+app.use(express.json());
+
 dotenv.config();
 
 dbConnect()
 
-app.get('/' , (req,res) => {
-    res.send('welcome')
-})
+app.use('/api/v1' , require('./routes/user.routes.js'))
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
