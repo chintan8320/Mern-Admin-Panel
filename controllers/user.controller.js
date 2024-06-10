992dc935f9124af62c4397ca826e40ac9fc5d60f
chintan mangroliya <chintan05ppsv2020@gmail.com>
2024-06-10 22:35:10 +0530

    changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -74,7 +74,7 @@ const loginUser = async (req, res) => {
         if (err) throw err;
         const expiryDate = new Date();
                 expiryDate.setDate(expiryDate.getDate() + 7);
-        res.cookie('jwtToken', token, { expires: expiryDate  ,httpOnly: true, secure: true, SameSite:'None'});
+        res.cookie('jwtToken', token, { expires: expiryDate  ,httpOnly: true, secure: true, sameSite:'Lax'});
         res.json({ token });
       }
     );
